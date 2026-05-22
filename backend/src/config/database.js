3a694f13500ee275d78224b644ae7759d6114daf
Alejandra117851 <mariapineda1104@gmail.com>
2026-05-22 15:42:14 -0500

    use database url

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-
-    ssl: process.env.NODE_ENV === 'production'
-        ? { rejectUnauthorized: false }
-        : false,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 
     max: 20,
     idleTimeoutMillis: 30000,
